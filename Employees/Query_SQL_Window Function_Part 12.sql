d5b4c3e7cd9817bf0c3bf65e26ef6b355947e7a6
RIFF7 <syarifudinjaelani@gmail.com>
2024-01-22 15:47:43 +0700

    Menyelesaikan 4 soal SQL terkait penerapan ROW_NUMBER(), RANK() dan DENSE_RANK

--- Employees/Query_SQL_Window Function_Part 12.sql
+++ Employees/Query_SQL_Window Function_Part 12.sql
@@ -719,13 +719,27 @@ values that employee number 10560 has ever signed a contract for.
 
 Order and display the obtained salary values from highest to lowest.
 */
+SELECT
+	emp_no,
+    salary,
+	ROW_NUMBER() OVER w AS row_num
+FROM salaries
+WHERE emp_no = 10560
+WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC);
 
 -- Exercise Number 2
 /*
 Write a query that upon execution, displays the number of salary 
 contracts that each manager has ever signed while working in the company.
 */
-
+SELECT
+	dm.emp_no,
+    COUNT(s.salary) AS no_of_salary_contracts
+FROM dept_manager dm
+JOIN salaries s ON dm.emp_no = s.emp_no
+GROUP BY 1
+ORDER BY 1;
+    
 -- Exercise Number 3
 /*
 Write a query that upon execution retrieves a result set containing 
@@ -734,6 +748,13 @@ Use a window function to rank all salary values from highest to lowest
 in a way that equal salary values bear the same rank and that gaps in 
 the obtained ranks for subsequent rows are allowed.
 */
+SELECT
+	emp_no,
+    salary,
+	RANK() OVER w AS rank_num
+FROM salaries
+WHERE emp_no = 10560
+WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC);
 
 -- Exercise Number 4
 /*
@@ -742,6 +763,12 @@ values that employee 10560 has ever signed a contract for. Use a window function
 to rank all salary values from highest to lowest in a way that equal salary 
 values bear the same rank and that gaps in the obtained ranks for subsequent rows are not allowed.
 */
-
+SELECT
+	emp_no,
+    salary,
+	DENSE_RANK() OVER w AS rank_num
+FROM salaries
+WHERE emp_no = 10560
+WINDOW w AS (PARTITION BY emp_no ORDER BY salary DESC);
 
 /*------------------------------------------------------------------------------------*/
