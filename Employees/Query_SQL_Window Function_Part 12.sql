94be00bc345dd4ac0a7bdf0d1471fecfb4c1a5d2
RIFF7 <syarifudinjaelani@gmail.com>
2024-01-22 15:15:13 +0700

    Menambahkan 4 soal SQL untuk meningkatkan pemahaman

--- Employees/Query_SQL_Window Function_Part 12.sql
+++ Employees/Query_SQL_Window Function_Part 12.sql
@@ -712,6 +712,36 @@ gunakan DENSE_RANK().
 
 */
 
+-- Exercise Number 1
+/*
+Write a query containing a window function to obtain all salary
+values that employee number 10560 has ever signed a contract for.
+
+Order and display the obtained salary values from highest to lowest.
+*/
+
+-- Exercise Number 2
+/*
+Write a query that upon execution, displays the number of salary 
+contracts that each manager has ever signed while working in the company.
+*/
+
+-- Exercise Number 3
+/*
+Write a query that upon execution retrieves a result set containing 
+all salary values that employee 10560 has ever signed a contract for. 
+Use a window function to rank all salary values from highest to lowest 
+in a way that equal salary values bear the same rank and that gaps in 
+the obtained ranks for subsequent rows are allowed.
+*/
+
+-- Exercise Number 4
+/*
+Write a query that upon execution retrieves a result set containing all salary 
+values that employee 10560 has ever signed a contract for. Use a window function 
+to rank all salary values from highest to lowest in a way that equal salary 
+values bear the same rank and that gaps in the obtained ranks for subsequent rows are not allowed.
+*/
 
 
 /*------------------------------------------------------------------------------------*/
