26232d434684f405f0ad3b788021721b8e7a1b48
RIFF7 <syarifudinjaelani@gmail.com>
2024-01-22 16:47:59 +0700

    Menambahkan soal baru untuk penggunaan RANK() dalam JOIN

--- Employees/Query_SQL_Window Function_Part 12.sql
+++ Employees/Query_SQL_Window Function_Part 12.sql
@@ -840,4 +840,31 @@ untuk setiap departemen berdasarkan gaji pegawai yang menjadi manajer departemen
 yang berada dalam rentang tanggal menjadi manajer departemen yang dimasukkan dalam hasil query.
 */
 
+-- Exercise Number 1
+/*
+Write a query that ranks the salary values in descending order of all contracts 
+signed by employees numbered between 10500 and 10600 inclusive. 
+Let equal salary values for one and the same employee bear the same rank. 
+Also, allow gaps in the ranks obtained for their subsequent rows.
+
+Use a join on the “employees” and “salaries” tables to obtain the desired result.
+*/
+
+-- Exercise Number 2
+/*
+Write a query that ranks the salary values in descending order 
+of the following contracts from the "employees" database:
+
+- contracts that have been signed by employees numbered 
+between 10500 and 10600 inclusive.
+
+- contracts that have been signed at least 4 full-years 
+after the date when the given employee was hired in the company for the first time.
+
+In addition, let equal salary values of a certain employee bear the same rank. 
+Do not allow gaps in the ranks obtained for their subsequent rows.
+
+Use a join on the “employees” and “salaries” tables to obtain the desired result.
+*/
+
 /*------------------------------------------------------------------------------------*/
