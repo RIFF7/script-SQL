d9d9e1fe309fe45e61572de994195d465544f9f7
RIFF7 <syarifudinjaelani@gmail.com>
2024-01-25 18:34:18 +0700

    Menyelesaikan 3 soal pada file Query_SQL_CTE_Part 13

--- Employees/Query_SQL_CTE_Part 13.sql
+++ Employees/Query_SQL_CTE_Part 13.sql
@@ -761,4 +761,96 @@ FROM employees e
 JOIN cte_f_highest_salary c2 ON e.emp_no = c2.emp_no
 CROSS JOIN cte_avg_salary c1;
 
+-- Exercise Number 1
+/*
+Use two common table expressions and a SUM() function in the SELECT statement 
+of a query to obtain the number of male employees whose highest salaries 
+have been below the all-time average.
+*/
+WITH cte_avg_salary AS(
+	SELECT
+		AVG(salary) AS avg_salary
+	FROM salaries
+),
+cte_m_highest_salary AS(
+	SELECT
+		s.emp_no,
+        MAX(s.salary) AS m_highest_salary
+	FROM salaries s
+    JOIN employees e ON s.emp_no = e.emp_no
+		AND e.gender = 'M'
+	GROUP BY 1
+)
+SELECT
+	SUM(
+		CASE
+			WHEN c2.m_highest_salary < c1.avg_salary THEN 1
+            ELSE 0
+		END
+    ) AS m_highest_salary_below_avg
+FROM employees e
+JOIN cte_m_highest_salary c2 ON e.emp_no = c2.emp_no
+JOIN cte_avg_salary c1;
+
+-- Exercise Number 2
+/*
+Use two common table expressions and a COUNT() function in the SELECT statement 
+of a query to obtain the number of male employees whose highest salaries 
+have been below the all-time average.
+*/
+WITH cte_avg_salary AS(
+	SELECT
+		AVG(salary) AS avg_salary
+	FROM salaries
+),
+cte_m_highest_salary AS(
+	SELECT
+		s.emp_no,
+        MAX(s.salary) AS m_highest_salary
+	FROM salaries s
+    JOIN employees e ON s.emp_no = e.emp_no
+		AND e.gender = 'M'
+	GROUP BY 1
+)
+SELECT
+	COUNT(
+		CASE
+			WHEN c2.m_highest_salary < c1.avg_salary THEN c2.m_highest_salary
+            ELSE NULL
+		END
+    ) AS max_salary
+FROM employees e
+JOIN cte_m_highest_salary c2 ON e.emp_no = c2.emp_no
+JOIN cte_avg_salary c1;
+
+-- Exercise Number 3
+/*
+Does the result from the previous exercise change if you used 
+the Common Table Expression (CTE) for the male employees' 
+highest salaries in a FROM clause, as opposed to in a join?
+*/
+WITH cte_avg_salary AS(
+	SELECT
+		AVG(salary) AS avg_salary
+	FROM salaries
+),
+cte_m_highest_salary AS(
+	SELECT
+		s.emp_no,
+        MAX(s.salary) AS m_highest_salary
+	FROM salaries s
+    JOIN employees e ON s.emp_no = e.emp_no
+		AND e.gender = 'M'
+	GROUP BY 1
+)
+SELECT
+	COUNT(
+		CASE
+			WHEN c2.m_highest_salary < c1.avg_salary THEN c2.m_highest_salary
+            ELSE NULL
+		END
+    ) AS max_salary
+FROM cte_m_highest_salary c2 
+JOIN cte_avg_salary c1;
+
 /*------------------------------------------------------------------------------------*/
