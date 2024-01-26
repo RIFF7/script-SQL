299e5d50246fda641f88a79876524d9e6dcafe44
RIFF7 <syarifudinjaelani@gmail.com>
2024-01-26 15:04:27 +0700

    Menyelesaikan soal pada file Query_SQL_Temporary_Table_Part 14.sql

--- Employees/Query_SQL_Temporary_Table_Part 14.sql
+++ Employees/Query_SQL_Temporary_Table_Part 14.sql
@@ -237,12 +237,19 @@ Create a temporary table called dates containing the following three columns:
 - another one displaying two months earlier than the current date and time, and a
 - third column displaying two years later than the current date and time.
 */
+CREATE TEMPORARY TABLE tempo_dates
+SELECT
+	NOW() AS current_date_and_time,
+    DATE_SUB(NOW(), INTERVAL 2 MONTH) AS two_months_earlier,
+    DATE_SUB(NOW(), INTERVAL -2 YEAR) AS two_years_later;
 
 -- Exercise Number 2
 /*
 Write a query that, upon execution, allows you to check the result 
 set contained in the dates temporary table you created in the previous exercise.
 */
+SELECT *
+FROM tempo_dates;
 
 -- Exercise Number 3
 /*
@@ -251,6 +258,14 @@ table you created during the previous lecture with a new
 Common Table Expression (CTE) containing the same columns. 
 Let all columns in the result set appear on the same row.
 */
+WITH cte_temp AS(
+	SELECT
+	NOW() AS current_date_and_time,
+    DATE_SUB(NOW(), INTERVAL 2 MONTH) AS two_months_earlier,
+    DATE_SUB(NOW(), INTERVAL -2 YEAR) AS two_years_later
+)
+SELECT *
+FROM tempo_dates td JOIN cte_temp ct;
 
 -- Exercise Number 4
 /*
@@ -258,10 +273,27 @@ Again, create a query joining the result sets from the dates temporary
 table you created during the previous lecture with a new Common Table Expression (CTE) 
 containing the same columns. This time, combine the two sets vertically.
 */
+WITH cte_temp AS(
+	SELECT
+	NOW() AS current_date_and_time,
+    DATE_SUB(NOW(), INTERVAL 1 MONTH) AS months_earlier,
+    DATE_SUB(NOW(), INTERVAL -1 YEAR) AS years_later
+)
+SELECT *
+FROM dates 
+UNION
+SELECT *
+FROM cte_temp;
 
 -- Exercise Number 5
 /*
 Drop the male_max_salaries and dates temporary tables you recently created.
 */
+
+-- Hapus Temporary Table male_max_salary
+DROP TEMPORARY TABLE IF EXISTS male_max_salary;
+
+-- Hapus Temporary Table tempo_dates
+DROP TEMPORARY TABLE IF EXISTS tempo_dates;
 
 /*------------------------------------------------------------------------------------*/
